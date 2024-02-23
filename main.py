a995319575be716b23ba5a18bb74e05f0e36d542
siamliam12 <129056535+siamliam12@users.noreply.github.com>
2024-02-23 18:49:06 +0600

    fixing issues
    know barebone it can extract data from the email

--- main.py
+++ main.py
@@ -7,26 +7,28 @@ import pandas as pd
 class EmailExtractor():    
     cwd = os.getcwd()
 
-    def details(subject_header,date=(datetime.datetime.now()-datetime.timedelta(1)).strftime("%d-%b-%Y")):
+    def details(self,subject_header,date=(datetime.datetime.now()-datetime.timedelta(1)).strftime("%d-%b-%Y")):
         #email search criteria
-        search_criteria = '(ON '+date+' Subject "'+subject_header+'")'
+        search_criteria = "(ON "+date+" Subject '"+subject_header+"')"
         return search_criteria
     
     def attachment_download(self,SUBJECT):
-        username = ''
-        password = ''
+        username = 'siamahmed222945@gmail.com'
+        password = 'jgdw kdtk gmbi ievx'
         url = 'imap.gmail.com'
         detach_dir = '.'#where to save attachments (default:current directory)
         #connecting to server
         mail = imaplib.IMAP4_SSL(url,993,timeout=60)
         mail.login(username,password)
+        print("login successful")
         mail.select()
         response,items = mail.search(None,SUBJECT)
         items = items[0].split() #getting the mails id
+        print(items)
         for emailId in items:
             response,data = mail.fetch(emailId,"(RFC822)")# fetching the mail, "`(RFC822)`" means "get the whole stuff", but you can ask for headers only, etc
             email_body = data[0][1] #getting the mail content
-            content = email.message_from_string(str(email_body))# parsing the mail content to get a mail object
+            content = email.message_from_bytes(email_body)# parsing the mail content to get a mail object
 
             #check if there is any attachments
             if content.get_content_maintype() != 'multipart':
@@ -53,9 +55,10 @@ class EmailExtractor():
                     fp = open(att_path,'wb')
                     fp.write(part.get_payload(decode=True))
                     fp.close()
-            print(str(filename)+' downloaded')
-            return filename
+                print(str(filename)+' downloaded')
+            # return filename
+           
         
 emailExtractor = EmailExtractor()
-subject ="testing" 
+subject ='SUBJECT "Testing"'
 emailExtractor.attachment_download(subject)
