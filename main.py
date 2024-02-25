b3593b97ed436dbc87c9942b414655e4b5f77b88
siamliam12 <129056535+siamliam12@users.noreply.github.com>
2024-02-25 16:15:43 +0600

    Added new features.
    Can extract email details using start date to end date as well as search by sender

--- main.py
+++ main.py
@@ -3,62 +3,102 @@ import os,glob
 import email
 import datetime
 import pandas as pd
+from dotenv import load_dotenv
+from bs4 import BeautifulSoup as bs
 
+load_dotenv()
 class EmailExtractor():    
     cwd = os.getcwd()
 
-    def details(self,subject_header,date=(datetime.datetime.now()-datetime.timedelta(1)).strftime("%d-%b-%Y")):
-        #email search criteria
-        search_criteria = "(ON "+date+" Subject '"+subject_header+"')"
+    # def details(self,subject_header,date=(datetime.datetime.now()-datetime.timedelta(1)).strftime("%d-%b-%Y")):
+    #     #email search criteria
+    #     search_criteria = "(ON "+date+" Subject '"+subject_header+"')"
+    #     return search_criteria
+    def details(self,subject_header,start_date=None,end_date=None):
+        search_criteria = "Subject '{}'".format(subject_header)
+        if start_date and end_date:
+            search_criteria += ' (Since {} Before {})'.format(start_date, end_date)
         return search_criteria
     
-    def attachment_download(self,SUBJECT):
-        username = 'siamahmed222945@gmail.com'
-        password = 'jgdw kdtk gmbi ievx'
+    def search_by_sender(self,sender):
+        search_criteria = "From '{}'".format(sender)
+        return search_criteria
+    
+    def attachment_download(self,search_criteria):
+        username = os.environ.get('username')
+        password = os.environ.get('password')
         url = 'imap.gmail.com'
-        detach_dir = '.'#where to save attachments (default:current directory)
+        detach_dir = './downloads'#where to save attachments (default:current directory)
         #connecting to server
         mail = imaplib.IMAP4_SSL(url,993,timeout=60)
         mail.login(username,password)
         print("login successful")
         mail.select()
-        response,items = mail.search(None,SUBJECT)
-        items = items[0].split() #getting the mails id
-        print(items)
-        for emailId in items:
-            response,data = mail.fetch(emailId,"(RFC822)")# fetching the mail, "`(RFC822)`" means "get the whole stuff", but you can ask for headers only, etc
-            email_body = data[0][1] #getting the mail content
-            content = email.message_from_bytes(email_body)# parsing the mail content to get a mail object
+        response,items = mail.search(None,search_criteria)
+        if response == 'OK':
+            items = items[0].split() #getting the mails id
+            for emailId in items:
+                response,data = mail.fetch(emailId,"(RFC822)")# fetching the mail, "`(RFC822)`" means "get the whole stuff", but you can ask for headers only, etc
+                email_body = data[0][1] #getting the mail content
+                content = email.message_from_bytes(email_body)# parsing the mail content to get a mail object
 
-            #check if there is any attachments
-            if content.get_content_maintype() != 'multipart':
-                continue
-            print(f"[{content["From"]}]:{content["Subject"]}")
+                #get other info
+                subject = content['Subject']
+                sender = content['From']
+                date = content['Date']
+                body = ''
 
-            for part in content.walk():
-                # multipart are just containers, so we skip them
-                if part.get_content_maintype() == 'multipart':
+                #check if there is any attachments
+                if content.get_content_maintype() != 'multipart':
                     continue
-                # is this part an attachment:
-                if part.get('Content-Disposition') is None:
-                    continue
-                filename = part.get_filename()
-                counter = 1
-                # if there is no filename, we create one with a counter to avoid duplicates
-                if not filename:
-                    filename = 'part-%03d%s' % (counter,'bin')
-                    counter += 1
-                att_path = os.path.join(detach_dir,filename)
 
-                #check if it's already there
-                if not os.path.isfile(att_path) :
-                    fp = open(att_path,'wb')
-                    fp.write(part.get_payload(decode=True))
-                    fp.close()
-                print(str(filename)+' downloaded')
-            # return filename
-           
+                for part in content.walk():
+                    # multipart are just containers, so we skip them
+                    if part.get_content_maintype() == 'multipart':
+                        continue
+                    # is this part an attachment:
+                    if part.get('Content-Disposition') is None:
+                        # Decode the payload
+                        payload = part.get_payload(decode=True)
+                        if payload:
+                            soup = bs(payload,'html.parser')
+                            # body += payload.decode('utf-8', 'ignore')
+                            body += soup.get_text()
+                    filename = part.get_filename()
+                    counter = 1
+                    if filename:
+                        attachments_ext = os.path.splitext(filename)[1]
+                        # Create folder according to attachment ext if it doesn't exist
+                        ext_folder = os.path.join(detach_dir, attachments_ext[1:])
+                        if not os.path.exists(ext_folder):
+                            os.makedirs(ext_folder)
+
+                        # Create filename if it doesn't exist
+                        if not filename:
+                            filename = 'part-%03d%s' % (counter, 'bin')
+                            counter += 1
+
+                        # Save the attachment in the extension folder
+                        att_path = os.path.join(ext_folder, filename)
+
+                        # Check if it's already there
+                        if not os.path.isfile(att_path):
+                            with open(att_path, 'wb') as fp:
+                                fp.write(part.get_payload(decode=True))
+                            print(f'{filename} downloaded')
+                print(f'''
+                    From : {sender}
+                    Subject : {subject}
+                    Date : {date}
+                    Email Body: {body}
+                    ''', )  
         
 emailExtractor = EmailExtractor()
-subject ='SUBJECT "Testing"'
-emailExtractor.attachment_download(subject)
+subject ='Testing'
+start_date = "01-Feb-2024"
+end_date = "28-Feb-2024"
+sender = ""
+# search_criteria = emailExtractor.details(subject,start_date,end_date)
+search_criteria = emailExtractor.search_by_sender(sender)
+print(search_criteria)
+emailExtractor.attachment_download(search_criteria)
